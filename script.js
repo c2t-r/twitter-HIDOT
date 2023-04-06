9e90af9db2ad0ee843ec0048ec6c698531eb547c
c2tr <80561604+c2tr-plugin@users.noreply.github.com>
2023-04-06 21:51:36 +0900

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-setInterval(()=>{
-    if (document.querySelector("[aria-label=\"アカウントメニュー\"] > div > div:nth-child(2)") !== null) {
-        document.querySelector("[aria-label=\"アカウントメニュー\"] > div > div:nth-child(2)").remove();
-        console.log("delete notify dot!")
-    }
-}, 30)
